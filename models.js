e3431dc52b8b774d25a32fb813ed833c68c43a67
Doyung <doyung@doyung-VirtualBox.(none)>
2013-02-12 18:31:11 -0500

    fixed colorcode

--- models.js
+++ models.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var UserSchema = mongoose.Schema({
 	facebookId: String,
-	colorCode: String
+	colorVal: String
 });
 
 var User = mongoose.model('User', UserSchema);
